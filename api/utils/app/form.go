4df483996e2ece3bb9365b815c780bfedd5e57dc
fajar septiawan <fajarsep12dev@gmail.com>
2020-05-03 20:09:42 +0700

    add app utils

--- api/utils/app/form.go
+++ api/utils/app/form.go
@@ -2,7 +2,7 @@ package app
 
 import (
 	"net/http"
-	c "github.com/fajarsep12dev/api/go-api/utils/constant"
+	C "github.com/fajarsep12dev/go-api/api/utils/constant"
 	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
 )
@@ -11,18 +11,18 @@ import (
 func BindAndValid(c *gin.Context, form interface{}) (int, int) {
 	err := c.Bind(form)
 	if err != nil {
-		return http.StatusBadRequest, c.Success
+		return http.StatusBadRequest, C.Success
 	}
 
 	valid := validation.Validation{}
 	check, err := valid.Valid(form)
 	if err != nil {
-		return http.StatusInternalServerError, c.Error
+		return http.StatusInternalServerError, C.Error
 	}
 	if !check {
 		MarkErrors(valid.Errors)
-		return http.StatusBadRequest, c.InvalidParam
+		return http.StatusBadRequest, C.InvalidParam
 	}
 
-	return http.StatusOK, c.Success
+	return http.StatusOK, C.Success
 }
---
+++ api/utils/app/msg.go
@@ -0,0 +1,20 @@
+package app
+
+import C "github.com/fajarsep12dev/go-api/api/utils/constant"
+
+// MsgFlags for mapping constant code
+var MsgFlags = map[int]string{
+	C.Success:      "Ok",
+	C.Error:        "Fail",
+	C.InvalidParam: "Invalid Parameters",
+}
+
+// GetMsg get error information based on Code
+func GetMsg(code int) string {
+	msg, ok := MsgFlags[code]
+	if ok {
+		return msg
+	}
+
+	return MsgFlags[C.Error]
+}
